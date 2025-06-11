630392f6c3240b13ab57f1dfabaf69d343d4a41c
William José <williamjmrosa@gmail.com>
2025-06-10 23:24:02 -0300

    Ajuste no exercicio 12

--- exercicio12.js
+++ exercicio12.js
@@ -1,19 +1,18 @@
 // 12. Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci.
 // Ex.: 1, 1, 2, 3, 5, 8, 13, 21.
 
-function fibonacci(elemento) {
+function fibonacci(elementos) {
 
-    if(elemento == 1 || elemento == 2) {
-        return 1
-    }else {
-        return fibonacci(elemento - 1) + fibonacci(elemento - 2)
+    let sequencia = [1, 1]
+
+    for (let i = 2; i < elementos; i++) {
+        sequencia.push(sequencia[i - 1] + sequencia[i - 2])
     }
+
+    return sequencia
+
 }
 
-let sequencia = []
-
-for(let i = 1; i <= 10; i++) {
-    sequencia.push(fibonacci(i))
-}
+let sequencia = fibonacci(10)
 
 console.log("A sequência de Fibonacci: ", sequencia.join(", "))
